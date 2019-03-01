cbee242a41cef8b0d57a565784939443a0122b98
Taylor Walker <taylorjameswalker@gmail.com>
2019-03-01 13:25:19 -0500

    Fix redirect

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ let startDay = moment("2019-02-28");
 let today = moment();
 let day = today.diff(startDay, "days") + 1;
 
-let dayNumber = window.location.href.match(new RegExp("/day/(.*)"))[1] || day;
+let dayNumber;
+try {
+  dayNumber = window.location.href.match(new RegExp("/day/(.*)"))[1];
+} catch {
+  dayNumber = -1;
+}
 
-let scripture = [];
+if (!isNaN(dayNumber) && +dayNumber > 0) {
+  let scripture = [];
 
-READING_PLAN.forEach(list => {
-  scripture.push(list[(dayNumber - 1) % list.length]);
-});
+  READING_PLAN.forEach(list => {
+    scripture.push(list[(dayNumber - 1) % list.length]);
+  });
 
-substring = "";
-scripture.forEach(chapter => {
-  substring += chapter + ", ";
-});
+  substring = "";
+  scripture.forEach(chapter => {
+    substring += chapter + ", ";
+  });
 
-let url = `https://www.biblegateway.com/passage/?search=${substring}&version=CSB`;
+  let url = `https://www.biblegateway.com/passage/?search=${substring}&version=CSB`;
 
-console.log(scripture);
-console.log(url);
+  console.log(scripture);
+  console.log(url);
+} else {
+  window.location.href = day;
+}
